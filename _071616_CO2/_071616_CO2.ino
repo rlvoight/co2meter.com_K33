a13aba0d11089754f87f5f2e5a9f7e58172fc36d
Kina Smith <kina.smith@gmail.com>
2016-07-17 18:10:52 +0000

    added notes about clock, removed the wakeSensor() function

--- _071616_CO2/_071616_CO2.ino
+++ _071616_CO2/_071616_CO2.ino
@@ -1,10 +1,9 @@
 #include <Wire.h>
-#include "Co2Meter_K33.h"
-#include <SPI.h> 
+#include <SPI.h>
 #include <SD.h>   //changed <> to ""
 #include "RTClib.h"
+#include "Co2Meter_K33.h"
 
-int co2Addr = 0x68;
 const int chipSelect = 10;
 const int motorPin = 4;
 RTC_DS1307 RTC; // define the Real Time Clock object
@@ -20,7 +19,7 @@ void setup() {
   // make sure that the default chip select pin is set to
   // output, even if you don't use it:
   pinMode(10, OUTPUT);
-  
+
   // see if the card is present and can be initialized:
   if (!SD.begin(chipSelect)) {
     Serial.println("Card failed, or not present");
@@ -32,24 +31,32 @@ void setup() {
   if (!RTC.begin()) {
     Serial.println("RTC Failed");
   }
+  /*to set the clock use this line of code
+   * but it will set it to the time on your computer
+   * So if you want it to convert correctly without a time zone offset
+   * you should set your time zone on your computer to be Greenich Mean Time or GMT-0
+   * 
+   * Once the time is set, you can comment this line out, the clock should stay set 
+   * and count accurately as long as the backup battery is in the shield.
+   */
+  
+  RTC.adjust(DateTime((__DATE__), (__TIME__))); //sets the RTC to the computer time.
 }
+
 void loop() {
   DateTime now; //creates a DateTime Object to hold the time. We will be storing data from clock here
 
   // We keep the sample period >25s or so, else the sensor will start ignoring sample requests. wakeSensor();
-  digitalWrite(motorPin, HIGH); //turns pump on
-  k33.initPoll(); //this is different? 
+  digitalWrite(motorPin, HIGH); //turns pump on.
   delay(60000); //wait one minute. rlv uncommented
+  k33.initPoll(); //This is basically wakes the sensor up
   delay(16000); //wait 16 seconds
-  wakeSensor();
   double tempValue = k33.readTemp();
   delay(20);
-  wakeSensor();
   double rhValue = k33.readRh();
   delay(20);
-  wakeSensor();
   double co2Value = k33.readCo2();
-  wakeSensor();
+
   now = RTC.now(); //reads current time from the Clock
   long timeStamp = now.unixtime(); //parses that DateTime Object into a UTC Timestamp
   if (co2Value >= 0) {
@@ -85,23 +92,8 @@ void loop() {
     }
   }
   digitalWrite(motorPin, LOW); //turning off motor
+
   for (int i = 0; i < 1; i++) {
     delay(60000);
   }
-}
-void wakeSensor() {
-  // This command serves as a wakeup to the CO2 sensor, for K33‐ELG/BLG Sensors Only
-  // You'll have the look up the registers for your specific device, but the idea here is simple:
-  // 1. Disabled the I2C engine on the AVR
-  // 2. Set the Data Direction register to output on the SDA line
-  // 3. Toggle the line low for ~1ms to wake the micro up. Enable I2C Engine
-  // 4. Wake a millisecond.
-
-  TWCR &= ~(1 << 2); // Disable I2C Engine
-  DDRC |= (1 << 4); // Set pin to output mode
-  PORTC &= ~(1 << 4); // Pull pin low
-  delay(1);
-  PORTC |= (1 << 4); // Pull pin high again
-  TWCR |= (1 << 2); // I2C is now enabled
-  delay(1);
 }
